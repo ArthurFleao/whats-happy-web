fb33dce218384c4a11a5bef1f5298eada6cbdacc
Arthur Leão <arthurfleao@gmail.com>
2020-05-18 19:44:24 -0300

    [INSTALL] Firebase e material

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase = {
+    apiKey: 'AIzaSyAvEDrBqbx0Wyvwa8gKDgdd4uE0OvntiKg',
+    authDomain: 'whats-happy-web.firebaseapp.com',
+    databaseURL: 'https://whats-happy-web.firebaseio.com',
+    projectId: 'whats-happy-web',
+    storageBucket: 'whats-happy-web.appspot.com',
+    messagingSenderId: '125543541966',
+    appId: '1:125543541966:web:db1492ff98b3b98f8f349f',
+    measurementId: 'G-5BGQBGKFJ5'
+  };
 };
 
 /*
