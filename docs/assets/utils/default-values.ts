75fb51d92b68fd281f32b45e7a7007d51f529f4e
Arthur Leão <arthurfleao@gmail.com>
2020-05-26 23:09:45 -0300

    [BUILD]

---
+++ docs/assets/utils/default-values.ts
@@ -0,0 +1,6 @@
+export const DefaultValues = Object.freeze({
+  tiposNegocio: ['negocio 1', 'negocio 2', 'negocio 3'],
+  tiposCredito: ['Financeiro', 'Comercial', 'Outros'],
+  situacaoCredito: ['Cobrança Ajuizada', 'Cobrança em Carteira', 'Cobrança com Terceiros', 'Outros'],
+  tipoGarantia: ['Sem Garantias', 'Fidejussórias', 'Real bens móveis', 'Real bens imóveis', 'outras']
+});
