7e1460d9beb023f7e446026c579d1dd2f0241cd7
Arthur Leão <arthurfleao@gmail.com>
2020-05-30 22:26:52 -0300

    [UPDATE] Menu lateral melhorado, sistema de paginação interna

--- src/app/shared/menu-lateral/menu-lateral.component.ts
+++ src/app/shared/menu-lateral/menu-lateral.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu-lateral',
@@ -6,58 +6,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-lateral.component.scss']
 })
 export class MenuLateralComponent implements OnInit {
+  sideMenuOpened = true; // estado atual do side menu (aberto/fechado)
+  showHeaders = true; // mostrar o menu?
+  perfilUser = 'paciente';
 
-  perfilUser = "paciente";
-
-  //array de objetos para criar itens de menu do psicologo
-    //perfilUsuario, descricao, link, icone
+  // array de objetos para criar itens de menu do psicologo
+  // perfilUsuario, descricao, link, icone
   menu = [
-    //itens de menu para ambos perfis de usuário
+    // itens de menu para ambos perfis de usuário
     {
       perfilUsuario: 'ambos',
       tituloItemMenu: 'Home',
       link: '/home',
       icone: 'home'
     },
-    //itens de menu do psicologo
-  {
-    perfilUsuario: 'psicologo',
-    tituloItemMenu: 'Cadastrar Paciente',
-    link: '/cadastro',
-    icone: 'person_add'
-  },
+    // itens de menu do psicologo
+    {
+      perfilUsuario: 'psicologo',
+      tituloItemMenu: 'Cadastrar Paciente',
+      link: '/cadastro',
+      icone: 'person_add'
+    },
 
-  {
-    perfilUsuario: 'psicologo',
-    tituloItemMenu: 'Lista de Pacientes',
-    link: '/login',
-    icone: 'list'
-  },
+    {
+      perfilUsuario: 'psicologo',
+      tituloItemMenu: 'Lista de Pacientes',
+      link: '/login',
+      icone: 'list'
+    },
 
-  {
-    perfilUsuario: 'psicologo',
-    tituloItemMenu: 'Relatórios',
-    link: '/login',
-    icone: 'source'
-  },
-  //itens de menu do paciente
-  {
-    perfilUsuario: 'paciente',
-    tituloItemMenu: 'Enviar relato',
-    link: '/enviarRelato',
-    icone: 'send'
-  },
-  //itens de menu para ambos perfis de usuário
-  {
-    perfilUsuario: 'ambos',
-    tituloItemMenu: 'Meus Dados',
-    link: '/perfil',
-    icone: 'person'
-  },
-]
+    {
+      perfilUsuario: 'psicologo',
+      tituloItemMenu: 'Relatórios',
+      link: '/login',
+      icone: 'source'
+    },
+    // itens de menu do paciente
+    {
+      perfilUsuario: 'paciente',
+      tituloItemMenu: 'Enviar relato',
+      link: '/enviarRelato',
+      icone: 'send'
+    },
+    // itens de menu para ambos perfis de usuário
+    {
+      perfilUsuario: 'ambos',
+      tituloItemMenu: 'Meus Dados',
+      link: '/perfil',
+      icone: 'person'
+    },
+  ];
+  isMobile: boolean;
 
 
-  constructor() { }
+  constructor() {
+    this.checkMenu();
+  }
+
+  @HostListener('window:resize', ['event']) // se o usuário mudar o tamanho de tela
+  onResize(event) {
+    this.checkMenu();
+  }
+
+  checkMenu() {
+    this.isMobile =  (window.innerWidth < 769); // o tamanho da tela é menor que 769? Se sim é mobile
+    this.sideMenuOpened = !this.isMobile; // side menu começa fechado se for mobile, ou aberto se for desktop.
+  }
 
   ngOnInit(): void {
   }
