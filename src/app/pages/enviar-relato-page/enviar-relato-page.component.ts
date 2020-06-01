13d4ba336792b2604816b766026de55b525cb60b
Bruno Guilherme Lunardi <nardi273@gmail.com>
2020-05-31 23:58:30 -0300

    [UPDATE] Envio de relatos para o Firebase (texto)

--- src/app/pages/enviar-relato-page/enviar-relato-page.component.ts
+++ src/app/pages/enviar-relato-page/enviar-relato-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SnackService } from 'src/app/services/snack.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AuthService } from './../../services/auth.service';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-enviar-relato-page',
@@ -7,9 +11,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnviarRelatoPageComponent implements OnInit {
 
-  constructor() { }
+  myUid: string; // uid do usuário
+
+  constructor(
+    private auth: AuthService,
+    private afs: AngularFirestore,
+    private snack: SnackService
+  ) {
+
+    this.auth.me().then(res => { // recupera info do usuario
+      this.myUid = res.uid; // salva uid do usuario logado
+    }, error => {
+      console.error(error);
+    });
+
+   }
 
   ngOnInit(): void {
+  }
+
+  enviarRelato(values){
+
+    console.log("values", values)
+
+    console.log("aqui")
+
+    //salva o relato na collection paciente => documento com a id do paciente que enviou o relato => subcollection relatos
+    this.afs.collection('paciente').doc(this.myUid).collection('relatos').add({
+
+      grauFelicidade: values.grauFelicidade,
+      grauDisposicao: values.grauDisposicao,
+      grauIrritabilidade: values.grauIrritabilidade,
+      relato: values.relato,
+      dataHora: firebase.firestore.FieldValue.serverTimestamp()
+
+    }).then((finli) => {
+      //this.loading = false; // indica que terminou de carregar
+      this.snack.success('Relato enviado com sucesso!');
+    }).catch((err) => {
+      console.error(err);
+    });
   }
 
 }
