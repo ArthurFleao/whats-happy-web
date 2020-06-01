1cc7764ba1560f0ef97f571556ffc224976b4a8b
Arthur Leão <arthurfleao@gmail.com>
2020-05-31 22:44:00 -0300

    import db

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from 'firebase-functions'; // importa funcoes
-import * as admin from 'firebase-admin' // importa ações de admin
+import * as admin from 'firebase-admin'; // importa ações de admin
+// admin.initializeApp(functions.config().firebase);
 admin.initializeApp();
+const db = admin.firestore();
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
@@ -14,23 +16,49 @@ export const register = functions.https.onRequest((request, response) => {
   if (request.method === 'OPTIONS') {
     response.status(200).send('só vai');
   } else {
-
     admin.auth().createUser({
-      email: "user@example.com",
+      email: request.body.email,
       emailVerified: false,
-      password: "secretPassword",
-      displayName: "John Doe",
-      photoURL: "http://www.example.com/12345678/photo.png",
-      disabled: false
+      password: request.body.password,
     })
       .then(function (userRecord) {
         // See the UserRecord reference doc for the contents of userRecord.
         response.status(200).send('Show!');
         console.log("Successfully created new user:", userRecord.uid);
+        // let transaction = db.runTransaction(t => {
+        //   return t.get(cityRef)
+        //     .then(doc => {
+        //       // Add one person to the city population.
+        //       // Note: this could be done without a transaction
+        //       //       by updating the population using FieldValue.increment()
+        //       let newPopulation = doc.data().population + 1;
+        //       t.update(cityRef, { population: newPopulation });
+        //     });
+        // }).then(result => {
+        //   console.log('Transaction success!');
+        // }).catch(err => {
+        //   console.log('Transaction failure:', err);
+        // });
       })
       .catch(function (error) {
         response.status(406).send('Já tem um cara com esse email bro!');
-        console.log("Error creating new user:", error);
       });
   }
 });
+
+export const getUserData = functions.https.onRequest((req, res) => {
+  res.set('Access-Control-Allow-Origin', '*'); // permtie CORS
+  if (req.method === 'OPTIONS') {
+    res.status(200).send('só vai');
+  } else {
+
+  }
+});
+// export const funcao = functions.https.onRequest((req, res) => {
+//   res.set('Access-Control-Allow-Origin', '*'); // permtie CORS
+//   if (req.method === 'OPTIONS') {
+//     res.status(200).send('só vai');
+//   } else {
+//     // CODIGO DA FUÇÃO
+//   }
+// });
